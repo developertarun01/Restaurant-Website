a49d9fc45d59f3273e441de7a7e5853b664415c7
Tarun <126320132+developertarun01@users.noreply.github.com>
2024-09-19 11:00:10 +0530

    Update StoreContext.jsx

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -22,7 +22,7 @@ const StoreContextProvider = (props) => {
     let totalAmount = 0;
     for (const item in cartItems) {
       if (cartItems[item] > 0) {
-        let itemInfo = food_list.find((product) => product._id === item);
+        let itemInfo = food_list.map((product) => product._id === item);
         totalAmount += itemInfo.price * cartItems[item];
       }
     }
